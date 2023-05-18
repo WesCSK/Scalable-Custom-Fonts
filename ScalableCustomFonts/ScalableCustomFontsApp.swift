85ef89eaf34fbb6512d9f78915329da33c276ddf
Swee Kwang <sweekwang94@gmail.com>
2023-05-18 21:16:18 +0800

    Initial Commit

---
+++ ScalableCustomFonts/ScalableCustomFontsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScalableCustomFontsApp.swift
+//  ScalableCustomFonts
+//
+//  Created by Swee Kwang Chua on 18/5/23.
+//
+
+import SwiftUI
+
+@main
+struct ScalableCustomFontsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
